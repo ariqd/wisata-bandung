6cc6c3a83e2ce5c59f6b2ba7f3145a462fbc31bf
ariqd <ariqdaffaathallah@gmail.com>
2025-01-10 14:40:31 +0700

    add images and scrolling

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -7,58 +7,64 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(top: 16),
-              child: const Text(
-                'Farm House Lembang',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Image.asset('images/farm-house.jpg'),
+              Container(
+                margin: const EdgeInsets.only(top: 16),
+                child: const Text(
+                  'Farm House Lembang',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 16.0),
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Icon(Icons.calendar_today),
-                      SizedBox(height: 8),
-                      Text('Open Everyday'),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.access_time),
-                      SizedBox(height: 8),
-                      Text('09.00 - 20.00'),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.monetization_on),
-                      SizedBox(height: 8),
-                      Text('Rp 25.000'),
-                    ],
-                  ),
-                ],
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 16.0),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Icon(Icons.calendar_today),
+                        SizedBox(height: 8),
+                        Text('Open Everyday'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.access_time),
+                        SizedBox(height: 8),
+                        Text('09.00 - 20.00'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.monetization_on),
+                        SizedBox(height: 8),
+                        Text('Rp 25.000'),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: const Text(
-                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16.0),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: const Text(
+                  'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16.0),
+                ),
               ),
-            )
-          ],
+              Image.network(
+                'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg',
+              ),
+            ],
+          ),
         ),
       ),
     );
